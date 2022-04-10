a7f88482b0e63dea55112b41b0ccb222ece97d4e
Sean Farley <sean@farley.io>
2022-04-09 22:24:15 -0500

    python: add initial implementation of general fixed point

--- beta_hypergraphs.py
+++ beta_hypergraphs.py
@@ -77,11 +77,91 @@ def beta_fixed_point(degrees, k, max_iter=500, tol=0.0001, beta=None):
     return beta
 
 
+def fixed_point_general(degrees, k_list, max_iter=500, tol=0.0001, beta=None):
+    """Use a fixed point algorithm to get the MLE."""
+    n = len(degrees)
+    if beta is None:
+        beta = np.zeros(n)
+
+    convergence = False
+    steps = 0
+
+    all_index_sets = list()
+    index_k = 0
+    prod_exp_beta_list = list()
+    for k in k_list:
+        index_k += 1
+        sets = list(itertools.combinations(range(n), k - 1))
+        all_index_sets.append(sets)
+        prod_exp_beta_list.append(np.ones(len(sets)))
+
+    while (not convergence and steps < max_iter):
+        exp_beta = np.exp(beta)
+        old_beta = beta.copy()
+        if any(np.isinf(old_beta)):
+            print("Infinite beta")
+            return
+
+        for index_k in range(len(k_list)):
+            sets = all_index_sets[index_k]
+            prod_exp_beta = prod_exp_beta_list[index_k]
+            for t in range(len(sets)):
+                tup = sets[t]
+                prod_exp_beta[t] = np.prod([exp_beta[tt] for tt in tup])
+                if np.isinf(prod_exp_beta[t]):
+                    print("Infinite beta")
+                    return
+
+            prod_exp_beta_list[index_k] = prod_exp_beta
+
+        for i in range(n):
+            sum_q_beta = list(range(len(k_list)))
+            for index_k in range(len(k_list)):
+                sets = all_index_sets[index_k]
+                prod_exp_beta = prod_exp_beta_list[index_k]
+                for j in range(len(sets)):
+                    tup = sets[j]
+                    if i not in tup:
+                        sum_q_beta[index_k] += (
+                            prod_exp_beta[j]
+                            / (1 + prod_exp_beta[j] * exp_beta[i])
+                        )
+                        if np.isinf(sum_q_beta[index_k]):
+                            print("Infinite beta")
+                            return
+            beta[i] = np.log(degrees[i]) - np.log(np.sum(sum_q_beta))
+
+        diff = max(abs(old_beta - beta))
+        # print(f"diff= {diff} -------- steps= {steps}")
+        # print(beta)
+        if diff < tol:
+            convergence = True
+
+        steps += 1
+    print(steps)
+    print(diff)
+    if steps == max_iter:
+        print("Max iterations reached")
+        return
+
+    return beta
+
+
 def main():
-    K53 = list(itertools.combinations([1, 2, 3, 4, 5], 3))
-    print(deg_seq(K53))
-    fp = beta_fixed_point(deg_seq(K53), k=3, max_iter=10000)
-    print(np.isclose(fp, 3.07028833 * np.ones(5)))
+    # K53 = list(itertools.combinations([1, 2, 3, 4, 5], 3))
+    # print(deg_seq(K53))
+    # beta_K53 = beta_fixed_point(deg_seq(K53), k=3, max_iter=10000)
+    # print(np.isclose(beta_K53, 3.07028833 * np.ones(5)))
+
+    d10_3 = (36, 36, 36, 36, 36, 36, 36, 36, 36, 36)
+
+    beta = fixed_point_general(d10_3, [3, ], max_iter=10000)
+    print(beta)
+
+    # [3.07028833 3.07028833 3.07028833 3.07028833 3.07028833 3.07028833
+    #  3.07028833 3.07028833 3.07028833 3.07028833]
+    # 3331
+    # 9.997253637461512e-05
 
 
 if __name__ == "__main__":
