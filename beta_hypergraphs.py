2054f85885c1c3606f437e96fd87ffe45a72723e
Sean Farley <sean@farley.io>
2022-04-10 21:43:23 -0500

    python: separate the R implementations into different functions

--- beta_hypergraphs.py
+++ beta_hypergraphs.py
@@ -19,6 +19,65 @@ def deg_seq(hyperedges, vertices=None):
     return d
 
 
+def beta_fixed_point_R(degrees, k, max_iter=500, tol=0.0001, beta=None):
+    """Use a fixed point algorithm to calculate the MLE."""
+    n = len(degrees)
+    if beta is None:
+        beta = np.zeros(n)
+
+    print(np.exp(beta))
+    convergence = False
+    steps = 0
+
+    # There are more efficient methods for calculating e^{beta_S} for each S in
+    # n\choose k-1, e.g using a dynamic algorithm
+    sets = list(itertools.combinations(range(n), k - 1))
+    prod_beta = np.ones(len(sets))
+
+    while(not convergence and steps < max_iter):
+        exp_beta = np.exp(beta)
+        old_beta = beta.copy()
+        if any(np.isinf(old_beta)):
+            return None
+
+        for i in range(len(sets)):
+            prod_beta[i] = np.prod([exp_beta[i] for i in sets[i]])
+
+            if np.isinf(prod_beta[i]):
+                print("Infinite beta")
+                return
+
+        for i in range(n):
+            sum_q = 0
+            for j in range(len(sets)):
+                tt = sets[j]
+                if i not in tt:
+                    sum_q += prod_beta[j] / (1
+                                             + prod_beta[j]
+                                             * exp_beta[i])
+                    if np.isinf(sum_q):
+                        print("Infinite beta")
+                        return
+
+            beta[i] = np.log(degrees[i]) - np.log(sum_q)
+
+        diff = max(abs(old_beta - beta))
+        # print(f"diff= {diff} -------- steps= {steps}")
+        # print(beta)
+        if diff < tol:
+            convergence = True
+
+        steps += 1
+
+    print(steps)
+    print(diff)
+
+    if steps == max_iter:
+        return
+
+    return beta
+
+
 def beta_fixed_point(degrees, k, max_iter=500, tol=0.0001, beta=None):
     """Use a fixed point algorithm to calculate the MLE."""
     n = len(degrees)
@@ -40,9 +99,6 @@ def beta_fixed_point(degrees, k, max_iter=500, tol=0.0001, beta=None):
         if any(np.isinf(old_beta)):
             return None
 
-        # for i in range(len(sets)):
-        #     prod_beta[i] = np.prod([exp_beta[i] for i in sets[i]])
-
         for i, s in enumerate(sets):
             prod_beta[i] = np.prod(exp_beta[np.asarray(s)])
 
@@ -51,14 +107,6 @@ def beta_fixed_point(degrees, k, max_iter=500, tol=0.0001, beta=None):
             return
 
         for i in range(n):
-            # sum_q = 0
-            # for j in range(len(sets)):
-            #     tt = sets[j]
-            #     if i not in tt:
-            #         sum_q += prod_beta[j] / (1
-            #                                  + prod_beta[j]
-            #                                  * exp_beta[i])
-
             ind = np.array([j for j in range(len(sets)) if i not in sets[j]])
             sum_q = np.sum(prod_beta[ind] / (1 + prod_beta[ind] * exp_beta[i]))
             if np.isinf(sum_q):
