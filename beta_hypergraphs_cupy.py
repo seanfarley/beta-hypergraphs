5abf9487df5fe5315de34c12857fff1aefb5d1e4
Sean Farley <sean@farley.io>
2022-04-21 13:29:46 -0500

    cupy: add initial version

---
+++ beta_hypergraphs_cupy.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python3
+
+import itertools
+import time
+
+import cupy as cp
+import numpy as np
+
+
+def deg_seq(hyperedges, vertices=None):
+    """Get the sequence of degrees."""
+    if vertices is None:
+        vertices = list(set(itertools.chain.from_iterable(hyperedges)))
+
+    n = len(vertices)
+    d = np.zeros(n)
+    for i in range(n):
+        for e in hyperedges:
+            if vertices[i] in e:
+                d[i] += 1
+
+    return d
+
+
+@cp.fuse
+def bfp_prodadd(exp_beta, ind3, iind3):
+    return exp_beta * ind3 + iind3
+
+
+@cp.fuse
+def bfp_prodexp(prod_beta, mask, exp_beta):
+    return (prod_beta * mask) / (1 + (prod_beta * mask * exp_beta))
+
+
+@cp.fuse
+def logdiff(ldegs, sum_q3):
+    return ldegs - cp.log(sum_q3)
+
+
+@cp.fuse
+def maxabs(old_beta, beta):
+    return cp.max(cp.abs(old_beta - beta))
+
+
+def beta_fixed_point(degrees, k, sets, max_iter=500, tol=0.0001, beta=None):
+    """Use a fixed point algorithm to calculate the MLE."""
+    n = len(degrees)
+    sn = len(sets)
+    an = cp.arange(n)
+    asn = cp.arange(sn)
+    if beta is None:
+        beta = cp.zeros(n)
+
+    convergence = False
+    steps = 0
+
+    # There are more efficient methods for calculating e^{beta_S} for each S in
+    # n\choose k-1, e.g using a dynamic algorithm
+
+    ind = cp.array([[j for j in asn if i not in sets[j]] for i in an])
+
+    ldegs = cp.log(degrees)
+
+    while(not convergence and steps < max_iter):
+        exp_beta = cp.exp(beta)
+        old_beta = beta.copy()
+        # if cp.any(np.isinf(old_beta)):
+        #     return None
+
+        prod_beta = cp.prod(exp_beta[sets], axis=1)
+
+        sum_q = cp.sum(prod_beta[ind]
+                       / (1 + (prod_beta[ind]
+                               * exp_beta.reshape(-1, 1))),
+                       axis=1)
+
+        beta = logdiff(ldegs, sum_q)
+        diff = maxabs(old_beta, beta)
+        # diff = cp.max(cp.abs(old_beta - beta))
+        if diff < tol:
+            convergence = True
+
+        steps += 1
+
+    # print(steps)
+    # print(diff)
+
+    if steps == max_iter:
+        return
+
+    return beta
+
+
+def beta_fixed_point2(degrees, k, sets, max_iter=500, tol=0.0001, beta=None):
+    """Alternative using less if-statements at the cost of more memory."""
+    n = len(degrees)
+    sn = len(sets)
+    an = cp.arange(n)
+    asn = cp.arange(sn)
+    if beta is None:
+        beta = cp.zeros(n)
+
+    convergence = False
+    steps = 0
+
+    # There are more efficient methods for calculating e^{beta_S} for each S in
+    # n\choose k-1, e.g using a dynamic algorithm
+
+    ind2 = cp.array([[i not in sets[j] for j in asn] for i in an])
+    iind3 = cp.transpose(ind2)
+    ind3 = ~iind3
+    # print(ind)
+    # print(ind2)
+    # print(~ind3.T == ind2)
+
+    # sets = cp.asarray(sets, dtype=int)
+    # print(sets)
+
+    # exp_beta = cp.exp(beta)
+    # exp_beta2 = exp_beta * ind3
+    # print(exp_beta[sets])
+    # print(exp_beta2)
+
+    # prod_beta = cp.prod(exp_beta[sets], axis=1)
+    # prod_beta2 = cp.prod(exp_beta2 + iind3, axis=1)
+    # print(prod_beta)
+    # print(prod_beta2)
+    # print(prod_beta[ind])
+    # # print(prod_beta * ind2)
+
+    ldegs = cp.log(degrees)
+
+    while(not convergence and steps < max_iter):
+        exp_beta = cp.exp(beta)
+        old_beta = beta.copy()
+        # if cp.any(np.isinf(old_beta)):
+        #     return None
+
+        pb = bfp_prodadd(exp_beta, ind3, iind3)
+        prod_beta = cp.prod(pb, axis=1)
+
+        pe = bfp_prodexp(prod_beta, ind2, exp_beta.reshape(-1, 1))
+        sum_q = cp.sum(pe, axis=1)
+
+        beta = logdiff(ldegs, sum_q)
+        diff = maxabs(old_beta, beta)
+        # diff = cp.max(cp.abs(old_beta - beta))
+        if diff < tol:
+            convergence = True
+
+        steps += 1
+
+    # print(steps)
+    # print(diff)
+
+    if steps == max_iter:
+        return
+
+    return beta
+
+
+def main():
+    # for correctness
+    n = 5
+    k = 3
+    K53 = list(itertools.combinations(range(n), k))
+    degs = deg_seq(K53)
+    sets = list(itertools.combinations(range(len(degs)), k - 1))
+
+    # K53_cp = cp.array(K53)
+    degs_cp = cp.array(degs)
+    sets_cp = cp.array(sets)
+
+    print(f"Running cupy vectorized code (with n={n}, k={k})")
+    tic = time.perf_counter()
+    beta_K53 = beta_fixed_point(degs_cp, k=k, sets=sets_cp, max_iter=10000)
+    toc = time.perf_counter()
+    print(f"beta_fixed_point took {toc - tic:0.4f} seconds")
+    print()
+
+    all_passed = cp.allclose(beta_K53, 3.07028833 * cp.ones(5))
+    print(f"Testing correctness for n={n}, k={k}; passing={all_passed}")
+    print()
+
+    print(f"Running cupy second vectorized code (with n={n}, k={k})")
+    tic = time.perf_counter()
+    beta_K53 = beta_fixed_point2(degs_cp, k=k, sets=sets_cp, max_iter=10000)
+    toc = time.perf_counter()
+    print(f"beta_fixed_point2 took {toc - tic:0.4f} seconds")
+    print()
+
+    # for performance
+    n = 25
+    k = 4
+    Kn53 = list(itertools.combinations(range(n), k))
+    degs = deg_seq(Kn53)
+    sets = list(itertools.combinations(range(len(degs)), k - 1))
+
+    # K53_cp = cp.array(K53)
+    degs_cp = cp.array(degs)
+    sets_cp = cp.array(sets)
+
+    print(f"Running python vectorized code (with n={n}, k={k})")
+    tic = time.perf_counter()
+    beta_fixed_point(degs_cp, k=k, sets=sets_cp, max_iter=10000)
+    toc = time.perf_counter()
+    print(f"beta_fixed_point took {toc - tic:0.4f} seconds")
+    print()
+
+    print(f"Running python second vectorized code (with n={n}, k={k})")
+    tic = time.perf_counter()
+    beta_fixed_point2(degs_cp, k=k, sets=sets_cp, max_iter=10000)
+    toc = time.perf_counter()
+    print(f"beta_fixed_point2 took {toc - tic:0.4f} seconds")
+
+
+if __name__ == "__main__":
+    main()
